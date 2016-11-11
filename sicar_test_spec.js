c77a2a1e3297b0f53c437e728ffbfd20336f53a4
Carlos Silva <cefs1984@gmail.com>
2016-11-11 17:32:03 -0300

    testando

--- sicar_test_spec.js
+++ sicar_test_spec.js
@@ -2,7 +2,7 @@ var frisby = require('frisby');
 
 var URL = 'http://car.semas.pa.gov.br/receptor/integracao/imoveis/informacoes?codigo=PA-1501907-0AC76504FB6342788991DDFBC7FCEFD4';
 
-frisby.create('SICAR: success')
+frisby.create('SICAR -- Status 200: success')
 	.get(URL)
 	.expectStatus(200)
 	.expectHeaderContains('content-type', 'application/json')
